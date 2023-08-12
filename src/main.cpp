f4d6cace6fe62437e55897585aa62c7d8b89992e
Li Keqing <me@kaze.ai>
2023-08-12 15:54:47 +0800

    Fix format

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@ BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD fdwReason, LPVOID lpvReserved)
 	case DLL_PROCESS_ATTACH:
 		missing_cameo::missing_cameo::get_instance().log("MissingCameo launching, rev {}", k_revision);
 		missing_cameo::missing_cameo::get_instance().set_module_handle(hInstance);
-		missing_cameo::missing_cameo::get_instance().log("Dll attached to process, hInstance = {}", (void*)hInstance);
+		missing_cameo::missing_cameo::get_instance().log("Dll attached to process, hInstance = {}", (void *)hInstance);
 		break;
 	case DLL_PROCESS_DETACH:
 		missing_cameo::missing_cameo::get_instance().log("Dll detached...");
